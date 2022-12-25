42a701f1443a3d8fa7613899dac5e4caae6e40f9
amkarnik (Amitkumar Karnik) <amkarnik@cisco.com>
2022-12-25 14:59:46 +0530

    fix issues

--- src/main/java/io/springworks/controller/BookController.java
+++ src/main/java/io/springworks/controller/BookController.java
@@ -3,9 +3,12 @@ package io.springworks.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -31,37 +34,31 @@ public class BookController {
 	private int bookId = 0000;
 
 	@GetMapping("/library")
-	public List<Book> getBooks(){
-		return bookService.getBooks();
+	public long getBooksCount(){
+		return bookService.getBooksCount();
 	}
 
 	// http://localhost:8080/api/v1/book/get?id=1
 	@GetMapping("/get")
 	public List<Book> getBook(@RequestParam("id") int id) {
 		logger.info("Requesting book with ID: {}", id);
-		List<Book> books = new ArrayList<Book>();
-		Book book = bookService.getbook(id);
-		if(book != null) {
-			logger.info("Returning book with ID: {}", id);
-			books.add(book);
-			return books;
-		}
+		List<Book> books = bookService.getbook(id);
 		return books;
 	}
 
 	@PostMapping("/add")
-	public BodyBuilder addBook(@RequestBody Book book){
+	public HttpStatus addBook(@RequestBody Book book){
 		bookId+=1;
 		book.setId(bookId);
 		bookService.addBook(book);
 		logger.info("Successfully added book to library: Book - Autor:{}, Name: {}", book.getAuthor(), book.getName());
-		return ResponseEntity.accepted();
+		return HttpStatus.ACCEPTED;
 	}
 
-	@DeleteMapping
-	public BodyBuilder deleteBook(Book book) {
-		bookService.deleteBook(book);
-		logger.info("Removed book - {} form library", book.getName());
-		return ResponseEntity.accepted();
+	@DeleteMapping("/delete")
+	public HttpStatus deleteBook(@RequestParam("id") int id) {
+		bookService.deleteBook(id);
+		logger.info("Removing book with id - {} form library", id);
+		return HttpStatus.OK;
 	}
 }
--- src/main/java/io/springworks/service/BookService.java
+++ src/main/java/io/springworks/service/BookService.java
@@ -1,12 +1,15 @@
 package io.springworks.service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import io.springworks.models.Book;
 import io.springworks.repos.BookRepo;
@@ -21,21 +24,21 @@ public class BookService {
 
 	private List<Book> books;
 
-	public List<Book> getBooks(){
-		books = new ArrayList<>();
-		bookRepo.findAll()
-		.forEach(books::add);
-		logger.info("Found {} books", books.size());
-		return books;
+	public long getBooksCount(){
+		long count = bookRepo.count();
+		logger.info("Found {} books", count);
+		return count;
+
 	}
 
-	public Book getbook(int id) {
-		Object book = bookRepo.findById(id);
-		if(book != null) {
+	public List<Book> getbook(int id) {
+		List<Book> books = new ArrayList<>();
+		Optional<Book> book = bookRepo.findById(id);
+		if(book.isPresent()) {
 			logger.info("Found book with ID: {}", id);
-			return (Book) book;
+			books.add(book.get());
 		}
-		return null;
+		return books;
 	}
 
 	public void addBook(Book book) {
@@ -43,8 +46,14 @@ public class BookService {
 		bookRepo.save(book);
 	}
 
-	public void deleteBook(Book book) {
-		logger.info("Deleting book by Author: {} Book name - {} from inventory", book.getAuthor(), book.getName());
-		bookRepo.delete(book);
+	public void deleteBook(int id) {
+		List<Book> books = getbook(id);
+		if(!CollectionUtils.isEmpty(books)){
+			Book book = books.get(0);
+			bookRepo.delete(book);
+			logger.info("Deleting book by Author: {} Book name - {} from inventory", book.getAuthor(), book.getName());
+			return;
+		}
+		logger.error("Could not find the book by ID: {}", id);
 	}
 }
