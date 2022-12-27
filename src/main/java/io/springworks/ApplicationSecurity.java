946cb33585172ebd1f7cb916f6076b81f54b9760
amkarnik (Amitkumar Karnik) <amkarnik@cisco.com>
2022-12-27 12:54:07 +0530

    Add Authorization for roles

--- src/main/java/io/springworks/ApplicationSecurity.java
+++ src/main/java/io/springworks/ApplicationSecurity.java
@@ -2,6 +2,7 @@ package io.springworks;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,16 +11,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// set in memory Auth
 		auth.inMemoryAuthentication()
-		.withUser("admin")
+		.withUser("amit")
 		.password("Spring")
-		.roles("admin");
+		.roles("USER")
+		.and()
+		.withUser("admin")
+		.password("admin")
+		.roles("ADMIN");
 	}
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();	// use different encoder for hashing your passwords
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.authorizeRequests()	// use least restricted role at top
+			.antMatchers("/library").hasRole("USER")
+			.antMatchers("/**").hasRole("ADMIN")
+			.and().formLogin();
 	}
 }
